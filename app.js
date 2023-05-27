d831501ba11182b56d75578f269611eba1e66b60
JuliaMaidan <115953663+JuliaMaidan@users.noreply.github.com>
2023-05-27 17:44:32 +0300

    add avatar

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+// const multer = require("multer");
+// const path = require("path");
+// const fs = require("fs").promises;
+// const { nanoid } = require("nanoid");
+
 require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
@@ -17,6 +22,43 @@ app.use(express.json());
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
+
+// const contacts = [];
+// const tempDir = path.join(__dirname, "temp");
+
+// const multerConfig = multer.diskStorage({
+//   destination: tempDir,
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({
+//   storage: multerConfig,
+// });
+
+// const contactsDir = path.join(__dirname, "public", "contacts");
+
+// app.post("/api/contacts", upload.single("cover"), async (req, res) => {
+//   const { path: tempUpload, originalname } = req.file;
+//   // console.log(req.file);
+//   // console.log(req.body);
+//   const resultUpload = path.join(contactsDir, originalname);
+//   await fs.rename(tempUpload, resultUpload);
+//   const cover = path.join("contacts", originalname);
+//   const newContact = {
+//     id: nanoid(),
+//     ...req.body,
+//     cover,
+//   };
+
+//   contacts.push(newContact);
+//   res.status(201).json(newContact);
+// });
+
+// app.get("api/contacts", async (req, res) => {
+//   res.json(contacts);
+// });
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });

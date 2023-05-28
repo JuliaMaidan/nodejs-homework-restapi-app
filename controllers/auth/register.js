b4f4702fb9ae011e6804b922698008491d6c7a90
JuliaMaidan <115953663+JuliaMaidan@users.noreply.github.com>
2023-05-28 21:29:55 +0300

    add updateAvatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
@@ -12,11 +13,13 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
   const result = await User.create({
     password: hashPassword,
     email,
     subscription,
+    avatarURL,
   });
   res.status(201).json({
     email: result.email,
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const { validateBody, putValidateBody } = require("./validateBody");
 
 const { authenticate } = require("./authenticate");
+const { upload } = require("./upload");
 
 module.exports = {
   validateBody,
   putValidateBody,
   authenticate,
+  upload,
 };
